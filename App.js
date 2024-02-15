f7d28684309364905054c46d4cf79e01e653e2e0
guduru <guduru.bhargav@datman.je>
2024-02-15 15:39:49 +0530

    added new things

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
-import {Text, View} from 'react-native';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 
 const App = () => {
+  const bhargavClick = () => {
+    console.log('handle navigation to another screen');
+  };
+
   return (
-    <View style={{alignItems: 'center'}}>
-      <Text style={{fontSize: 40}}>{'bhargav'}</Text>
+    <View
+      style={{alignItems: 'center', justifyContent: 'center', height: '100%'}}>
+      <TouchableOpacity onPress={bhargavClick()}>
+        <Text style={{fontSize: 40}}>{'bhargav'}</Text>
+      </TouchableOpacity>
+      <TextInput
+        placeholder={'enter your field name'}
+        placeholderTextColor={'red'}
+        selectionColor={'blue'}
+        style={{
+          borderColor: 'yellow',
+          borderWidth: 2,
+          width: '80%',
+        }}
+      />
     </View>
   );
 };
