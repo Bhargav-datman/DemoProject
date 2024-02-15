d19ba0364fdaa3e376a2f23b9039d2cbcc1cfdf8
guduru <guduru.bhargav@datman.je>
2024-02-15 13:35:05 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{alignItems: 'center'}}>
+      <Text style={{fontSize: 40}}>{'bhargav'}</Text>
+    </View>
+  );
+};
+export default App;
